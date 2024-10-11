0b6f0b39e8ccb95edff5db8f15a7173c9f7975e9
Meena Kumari <meenasahu8827@gmail.com>
2024-10-11 20:58:06 +0530

    Update Signup.js
    Add render backend url

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -14,7 +14,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/users/signup', {
+      const response = await axios.post('https://workout-tracker-backend-3.onrender.com/api/users/signup', {
         name,
         email,
         password,
